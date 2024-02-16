729cc38cd37b74914b7b6faaa3a0430217359c2e
Ramani Viswanathan <97403814+Ramani-Viswanathan@users.noreply.github.com>
2024-02-15 19:47:12 -0600

    Feb Assignment 1

--- src/pproj1_mergefiles.py
+++ src/pproj1_mergefiles.py
@@ -1,26 +1,38 @@
 '''
 PROJECT DETAILS : 
-A MINI PROJECT USING PANDAS TO COMBINE 2 FILES AND SHARE OUTPUT OF THE 
-SELECTED COLUMN IN A SINGLE FILE OT TABLE FORMAT
-Data Source : https://www.kaggle.com/datasets/dillonmyrick/high-school-student-performance-and-demographics?resource=download
-High School Student Performance & Demographics
-High school student perfomance as well as demographic, social, and parent data.
+PySpark February 2024 Batch – Assignment 1
 
-File Names : student_portuguese_clean.csv and student_math_clean
+Project work on pandas:
+Id	Batch start date	Batch end date	Email	Name	Employee id
+
+Create data directory and create 3 excel files with the above format
+Put some dummy data in each file
+Write pandas code (.py) to read each excel files and combine the data from these 3 input file.
+Select only ID, email, Name and generate an output excel file with the combined data
+
 
 '''
-
+# import panda library
 import pandas as pd
 
-#Read data from File stored in 'Google Drive / data' directory
-df = pd.read_csv("data/student_math_clean.csv")
-df1 = pd.read_csv("data/student_portuguese_clean.csv")
+# Read data from File stored in ' data' directory, assigned to Data Frame df1, df2,df3
+df1 = pd.read_excel("data/training_batch_1.xlsx")
+df2 = pd.read_excel("data/training_batch_2.xlsx")
+df3 = pd.read_excel("data/training_batch_3.xlsx")
+
 #select only required columns
-selected_col = ["student_id","school","sex","age","address_type","final_grade"]
-# display only selected columns
-df_selected_columns = df[selected_col]
+selected_col = ["id","email","name"]
+
+# add the selected column in data frame
 df_selected_columns1 = df1[selected_col]
-#show the selected column where 'student_id' is same
-df_merge = pd.merge(df_selected_columns,df_selected_columns1, on="student_id", how="left")
-# print Merged data.
-print(df_merge)
+df_selected_columns2 = df2[selected_col]
+df_selected_columns3 = df3[selected_col]
+
+#Append all 3 daata frame into 1 data frame
+df_append = pd.concat([df_selected_columns1, df_selected_columns2, df_selected_columns3],ignore_index=True)
+
+#write to excel file, create a file in google drive
+df_append.to_excel("data/training_batch_consolidated.xlsx",sheet_name='Consolidated')
+
+#print the appended output for validation
+print (df_append)
